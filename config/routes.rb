a5754c293cffc85fa0dc2fa878eb6aeffe1491cd
Manish0407 <manishp@shriffle.com>
2022-08-19 17:36:55 +0530

    this is lms_day1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users
+  root "home#index"
   get 'home', to: 'home#index'
 end
--- db/migrate/20220818145037_devise_create_users.rb
+++ db/migrate/20220818145037_devise_create_users.rb
@@ -3,10 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      ## Database authenticatable
+      ## Database authenticatable 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
