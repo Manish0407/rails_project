98a8d93419a00d98b8ab6d0d61930a9315af6de4
Manish0407 <manishp@shriffle.com>
2022-09-12 20:27:10 +0530

    this is from day16

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,13 @@
 class Book < ApplicationRecord
   has_many :book_issues
-  has_many :users, through: :book_issues
-  before_validation :normalize_name, on: :create 
+  has_many :users, through: :book_issues, :dependent => :destroy
+  before_save :normalize_name
   validates :name, presence: true, uniqueness: { scope: :book_author }
   validates :qty, presence: true, length: { minimum: 1 }
 
   private
     def normalize_name
-      self.name = name.downcase.titleize, 
+      self.name = name.downcase.titleize 
       self.book_author = book_author.downcase.titleize
     end 
 end
--- app/models/book_issue.rb
+++ app/models/book_issue.rb
@@ -2,16 +2,15 @@ class BookIssue < ApplicationRecord
   belongs_to :user
   belongs_to :book
   enum :status, [:request, :allot, :cancel, :return, :deposit]
-  validates :book_id, presence: true#, uniqueness: { scope: :user_id,}
+  validates :book_id, presence: true#, uniqueness: { scope: :user_id if status: != "deposit"}
   # attr_accessor :status
 
-  # before_save :check_status
-  # private
+#   before_save :check_status
+#   private
 
-  # def check_status
-  #   unless self.status_previous_change
-      
-  #   end
-  #   status.changed? 
-  # end
+#   def check_status
+#     unless self.status_previous_changes
+    
+#     end 
+#   end
 end
